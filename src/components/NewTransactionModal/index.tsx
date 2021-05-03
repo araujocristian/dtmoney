15fcc2127373afdbc3c25f0fc289e21383572aca
Cristian Araujo <araujocristian@live.com>
2021-05-02 21:43:34 -0300

    feat: Inserindo transação na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,15 +3,12 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
-}
-
-function handleCreateNewTransaction(event: FormEvent) {
-  event.preventDefault();
 }
 
 export function NewTransactionModal({
@@ -22,6 +19,19 @@ export function NewTransactionModal({
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+
+    api.post("transactions", data);
+  }
 
   return (
     <Modal
